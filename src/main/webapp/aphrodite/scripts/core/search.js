cd2316e3b61b28c393448a3f3b3ed0205f3d69b3
vhypnus <vhypnus@gmail.com>
2015-07-16 17:31:49 +0800

    change

--- src/main/webapp/aphrodite/scripts/core/search.js
+++ src/main/webapp/aphrodite/scripts/core/search.js
@@ -2,29 +2,46 @@
     var Search = function(binding){
         this.method = binding.attr("id") ;
         this.binding = binding ;
+        this.records = undefined ;
         this._init();
 
     } ;
     Search.prototype = {
         _init:function(){
+        this._initEvent();
         },
         _initEvent:function(){
-            var jqbtn = this.binding.next("div").find("button");
-            $(jqbtn).on("click",function(e){
+            this.binding.on("keyup",function(e){
 
+                //alert($(this).val())
+                var _jq = $(this) ;
+                _jq.getSearch().filter(_jq.val()) ;
+
+               _jq.getSearch().binding.next("div").find("ul").dropdown("toggle")
             }) ;
         },
+        filter:function(value){
+            var arr = new Array();
+            $.each(this.records,function(index, record){
+                if(value == record.name){
+                    arr[arr.length] = record ;
+                }
+            }) ;
+
+            this.setData(arr) ;
+        },
         setData:function(records){
+            this.records = records ;
             if(records == undefined || records.length == 0){
                 return ;
             }
 
             var selhtml = "" ;
-           $.each(records,function(index,record){
+            $.each(records,function(index,record){
                 selhtml += "<li><a href=\"#"+record.code+"\">"+record.name+"<a></li>" ;
             });
 
-            this.binding.next("div").find("ul").append(selhtml) ;
+            this.binding.next("div").find("ul").empty().append(selhtml) ;
             this.binding.next("div").find("ul").find("li").find("a").on("click",function(e){
                 var _jq = $(this) ;
                 var input=_jq.parents("div.input-group-btn").prev("input[type=search]").getField() ;
@@ -42,7 +59,7 @@
             return search ;
         },
         getSearch:function(){
-
+            return this.data("search") ;
         }
     });
 })();
